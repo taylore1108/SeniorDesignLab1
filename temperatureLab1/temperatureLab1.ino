932c0f9c30da7519bbf425a63cb963c9c06b17a8
taylore1108 <82013210+taylore1108@users.noreply.github.com>
2022-09-18 15:42:40 -0500

    small updates

--- temperatureLab1/temperatureLab1.ino
+++ temperatureLab1/temperatureLab1.ino
@@ -1,17 +1,23 @@
 #include <LiquidCrystal.h>
+#include <SPI.h>
+#include <WiFi.h>
 
+//hardware set up
 const int buttonPin = 7; // digital pin for push button
 const int switchPin = 6; // digital pin for the switch
 const int lcdPin = 8; // this is where the LCD power goes
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; // follow link in google doc for wiring
+const byte ip[] = { 192, 168, 0, 50 };
+const byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x85, 0xD9 };
 
+//code set up
 int buttonState = 0; // state of the button
 float probetemp = 0; // this is the probe temp value we read
 boolean changetoF = false; // do we want to convert probe temp in C to temp in F? yes = true, no = false
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-  // put your setup code here, to run once:
+  //todo add 
   pinMode(buttonPin, INPUT);
   pinMode(switchPin, INPUT);
   pinMode(lcdPin, OUTPUT);
@@ -22,7 +28,6 @@ void loop() {
   // put your main code here, to run repeatedly:
   buttonState = digitalRead(switchPin);
   if(digitalRead(switchPin) == HIGH){
-    probetemp = changeReading(readProbe(), changetoF);
     
     buttonState = digitalRead(buttonPin);
     //todo update server here and 
@@ -31,10 +36,9 @@ void loop() {
   
     if (buttonState == HIGH) { // we only want to display locally when the button is pressed
       powerLCD(true);
-      lcd.setCursor(0, 1); //todo double check this prints in the right spot 
-      lcd.print(probetemp);
-      if (changetoF) lcd.print("F");
-      else lcd.print("C");
+      lcd.clear();
+      lcd.setCursor(0, 0); //todo double check this prints in the right spot 
+      lcd.print(changeReading(readProbe(), changetoF));
     
     }else{ 
       powerLCD(false);
@@ -46,14 +50,21 @@ void loop() {
 
 float readProbe(){
   //todo read probe temperature in celcius here
+
+  //if disconnected
   return -1;
 }
 
-float changeReading(float temp, boolean inFahrenheit){ //this is done
-  if(inFahrenheit){
-    return (temp * 9/5)+32;
+String changeReading(float temp, boolean inFahrenheit){ //this is done
+
+  if(temp == -1){
+    return "Unplugged Sensor";
   }
-  return temp;
+  
+  if(inFahrenheit){
+    return String((temp * 9/5)+32) + "F";
+  }
+  return String(temp) + "C";
 }
 
 void powerLCD(boolean power){ // todo test this

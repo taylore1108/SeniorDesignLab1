5c3a7bd7f8ee1cd45789ac400b311d2e9cc96389
taylore1108 <82013210+taylore1108@users.noreply.github.com>
2022-09-29 22:45:27 -0500

    magik

--- temperatureLab1/temperatureLab1.ino
+++ temperatureLab1/temperatureLab1.ino
@@ -200,6 +200,8 @@ char webpage[] PROGMEM = R"=="==(
   </body>
 </html>
 )=="==";
+
+
 WiFiMulti wifiMulti;
 //hardware set up
 int buttonPin = 35; // digital pin for push button
@@ -207,11 +209,15 @@ const int switchPin = 34; // digital pin for the switch
 const int lcdPin = 13; // this is where the LCD power goes
 const int rs = 14, en = 27, d4 = 26, d7 = 32, d6 = 33, d5 = 25; // follow link in google doc for wiring
 const byte ip[] = { 192, 168, 0, 50 };
+
+int switchVal =-1;
+int buttonVal=-1;
+String tempVal = "0";
 const byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x85, 0xD9 };
 
 //code set up
 int buttonState = 0; // state of the button
-float probetemp = 0; // this is the probe temp value we read
+String probetemp = "0"; // this is the probe temp value we read
 boolean changetoF = false; // do we want to convert probe temp in C to temp in F? yes = true, no = false
  LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 //sms set up 
@@ -219,8 +225,8 @@ const char* resource = "https://maker.ifttt.com/trigger/TempTrigger/with/key/fOZ
 //const char* server = "maker.ifttt.com";
 String numberToText = "16308006164";
 String messageToText = "This is a text of the arduino functions";
-int TextTopBoundry;
-int TextBottomBoundry;
+int TextTopBoundryInC = 23; // keep in C
+int TextBottomBoundryInC = 25; // keep in C
 const char* ssid     = "Galaxy S2267C8";
 const char* password = "nxzz5758";
 AsyncWebServer server(80);
@@ -235,38 +241,38 @@ void setup() {
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
   lcd.clear();
-  lcd.print("Hello World");
- 
+  //lcd.print("Hello World");
   initWiFi();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  probetemp = changeReading(readProbe(), changetoF);
+  if(tempVal != probetemp){
+    tempVal = probetemp;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(String(probetemp));
+  }
   buttonState = digitalRead(switchPin);
-  String s = String( buttonState);
-  
-  //USE_SERIAL.printf("[DEBUG] SWITCH %s...\n",s);
+  if(switchVal!=buttonState){
+    switchVal = buttonState;
+    String s = String( switchVal);
+    USE_SERIAL.printf("[DEBUG] SWITCH %s...\n",s);
+  }
   if(digitalRead(switchPin) == HIGH){
-    
     buttonState = digitalRead(buttonPin);
-    String s = String(buttonState);
-    // USE_SERIAL.printf("[DEBUG] BUTTON %s...\n",s);
-    //todo update server here and 
-    //read is "virtual button" is pressed
-    //if("vitrualButton == HIGH) buttonState =HIGH;
-  
-    if (buttonState == HIGH) { // we only want to display locally when the button is pressed
-      powerLCD(true);
-     // lcd.clear();
-      //lcd.setCursor(0, 0); //todo double check this prints in the right spot 
-      //lcd.print(changeReading(readProbe(), changetoF));
-      lcd.print("Hello World");
-    }else{ 
-      powerLCD(false);
-     // lcd.clear();
-      lcd.print("Hello World");
+    if(buttonVal!=buttonState){
+      buttonVal = buttonState;
+      String s = String(buttonVal);
+      USE_SERIAL.printf("[DEBUG] BUTTON %s...\n",s);
+      if (buttonState == HIGH) { 
+        powerLCD(true);
+      }else{ 
+        powerLCD(false);
+      }
     }
   }
+ delay(100);
 }
 
 void initWiFi() {
@@ -279,13 +285,7 @@ void initWiFi() {
         USE_SERIAL.flush();
         delay(1000);
     }
-
-//OLD text sms stuff
-    //wifiMulti.addAP(ssid, password);
-     //if((wifiMulti.run() == WL_CONNECTED)) {
-     //}else{
-     // Serial.println("error while connecting using wifiMulti");
-     //}
+    
     WiFi.begin(ssid,password);
     while (WiFi.status() != WL_CONNECTED) {
       delay(1000);
@@ -333,11 +333,15 @@ String processor(const String& var){
 
 
 void sendTextTest2(){
+  //wifiMulti.addAP(ssid, password);
+  //if((wifiMulti.run() == WL_CONNECTED)) {
       HTTPClient http;   
       http.begin(resource);  
-      http.addHeader("Content-Type", "application/json");       
-      int httpResponseCode = http.POST("{\"value1\":\"Message body here\",\"value2\":\"16308006164\"}");
-USE_SERIAL.println("Response Code: "+httpResponseCode);
+      http.addHeader("Content-Type", "application/json");    
+      String packet = "{\"value1\":\""+messageToText+"\",\"value2\":\""+numberToText+"\"}";
+      int httpResponseCode = http.POST(packet);
+      //int httpResponseCode = http.POST("{\"value1\":\"Message body here\",\"value2\":\"16308006164\"}");
+      USE_SERIAL.println("Response Code: "+httpResponseCode);
    
 //  StaticJsonDocument<200> doc;
 //  doc["Value1"] = messageToText;
@@ -345,27 +349,41 @@ USE_SERIAL.println("Response Code: "+httpResponseCode);
 //   
 //  String requestBody;
 //  serializeJson(doc, requestBody);
-   
- //int httpResponseCode = http.POST(requestBody);
+ // int httpResponseCode = http.POST(requestBody);
 
-      if(httpResponseCode > 0) { String response = http.getString();                         
-      http.end();
-      delay(5000);
-    }
+      //if(httpResponseCode > 0) { 
+        String response = http.getString();         
+        USE_SERIAL.println("Response Code2: "+response);                
+        http.end();
+      //}
+
+//    }
+//    else{
+//        Serial.println("error while connecting using wifiMulti");
+//    }
 }
 
 float readProbe(){
-  DS18B20 ds(15); //sets the temp probe to pin 6
+  DS18B20 ds(4); //sets the temp probe to pin 4
   float temp = ds.getTempC(); //gets the temperature of the probe
-  USE_SERIAL.println(String(temp,2));
+  USE_SERIAL.println(String(temp));
   return temp;
 }
 
-String changeReading(float temp, boolean inFahrenheit){ //this is done
+String changeReading(float temp, boolean inFahrenheit){ //add savinf data here
 
-  if(temp == -0.06){
+  if(temp < 1){
     return "Unplugged Sensor";
   }
+
+//  if(temp < TextBottomBoundryInC){
+//    messageToText = "Temperature Probe detects temperature " +String(temp)+ "C, which is below lower boundry "+ String(TextBottomBoundryInC) +" C"; 
+//    sendTextTest2();
+//  }
+//  else if( temp > TextTopBoundryInC){
+//    messageToText = "Temperature Probe detects temperature " +String(temp)+ "C, which is above upper boundry "+ String(TextBottomBoundryInC) +" C"; 
+//    sendTextTest2();
+//  }
   
   if(inFahrenheit){
     return String((temp * 9/5)+32) + "F";
